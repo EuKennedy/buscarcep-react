0e4ef7b24ab120550e7e82dccf7907599d128cd4
EuKennedy <kennedy.rodrigues1104@gmail.com>
2023-09-14 15:31:50 -0300

    I start my project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BsSearch } from "react-icons/bs";
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className="title">Buscador de CEP</h1>
+        
+        <div className="containerInput">
+          <input type="text" placeholder="Digite seu CEP:"></input>
+          <button className="buttonSearch">
+            <BsSearch size={25} color="#00000"/>
+          </button>
+        </div>
+        
+        <main className="main">
+          <h2>CEP: 317411667</h2>
+          <span>Rua pindamonhangaba</span>
+          <span>Complemente: Casa</span>
+        </main>
+        
     </div>
   );
 }
