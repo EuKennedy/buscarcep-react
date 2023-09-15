fe25aeb8d8fa28f25b67ab38c2c04173302fe3b6
EuKennedy <kennedy.rodrigues1104@gmail.com>
2023-09-15 17:56:35 -0300

    The project finished, now just make a error tratament and others simple functions.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+//JAVA SCRIPT HERE
+
+import {useState} from 'react'
 import { BsSearch } from "react-icons/bs";
 import './style.css';
+import api from './Api.js/api';
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({});
+  const [rua, setRua] = useState({});
+  const [bairro, setBairro] = useState({});
+  const [uf, setEstado] = useState({});
+  
+  async function clickSearch() {
+    
+    if (input === '') {
+      alert('Preencha algum cep na caixa.')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);  
+      setCep(response.data);
+      setRua(response.data);
+      setBairro(response.data);
+      setEstado(response.data);
+      setInput("");
+    } catch (error) {
+      alert('Ocorreu um erro ao buscar esse CEP');
+      setInput("");
+    }
+
+  }
+
+  //start of html
   return (
     <div className="container">
-        <h1 className="title">Buscador de CEP</h1>
-        
+        <h1 className="title">Buscador de CEP</h1>      
         <div className="containerInput">
-          <input type="text" placeholder="Digite seu CEP:"></input>
-          <button className="buttonSearch">
-            <BsSearch size={25} color="#00000"/>
+          <input type="text" placeholder="Digite seu CEP:" value={input} 
+          onChange={(e) => setInput(e.target.value)}
+          />
+          
+          <button className="buttonSearch" onClick={clickSearch}>
+            <BsSearch size={30} color="#00000"/> 
           </button>
         </div>
         
         <main className="main">
-          <h2>CEP: 317411667</h2>
-          <span>Rua pindamonhangaba</span>
-          <span>Complemente: Casa</span>
+          <h2 id='cep'>CEP: {cep.cep}</h2>
+          <span id='rua'>Rua: {rua.logradouro}</span>
+          <span id='bairro'>Bairro: {bairro.bairro}</span>
+          <span id='uf'>Estado: {uf.uf}</span>
         </main>
     </div>
   );
 }
-
 export default App;
